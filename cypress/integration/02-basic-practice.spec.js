7de7723624d175a0cc843fa32190c29bf2667215
ioannaxen <github@ioanna.dev>
2022-04-04 11:45:25 +0100

    :white_check_mark: Updating basic practice tests after seeing answers

--- cypress/integration/02-basic-practice.spec.js
+++ cypress/integration/02-basic-practice.spec.js
@@ -7,41 +7,47 @@ describe('Basic Practice', () => {
 
   describe('Adding a new item', () => {
     it('should put a new item on the page after clicking on "Add Item"', () => {
-      cy.get('[data-test="new-item-input"]')
-        .type('Good attitude')
-        .should('have.value', 'Good attitude')
-        .click(cy.get('[data-test="add-item"]'));
+      const item = 'Good attitude';
+
+      cy.get('[data-test="new-item-input"]').type(item);
+      cy.get('[data-test="add-item"]').click();
+
       cy.get('[data-test="new-item-input"]').should('not.have.value', 'Good attitude');
-      cy.contains('Good attitude');
+      cy.contains(item);
     });
 
     it('should put a new item in the "Unpacked Items" list', () => {
-      cy.get('[data-test="new-item-input"]')
-        .type('Good attitude')
-        .click(cy.get('[data-test="add-item"]'));
-      cy.get('[data-test="items-unpacked"]').contains('Good attitude');
+      const item = 'Good attitude';
+
+      cy.get('[data-test="new-item-input"]').type(item);
+      cy.get('form').submit();
+
+      cy.get('[data-test="items-unpacked"]').contains(item);
     });
 
     it('should put a new item as the last item in the "Unpacked Items" list', () => {
-      cy.get('[data-test="new-item-input"]')
-        .type('Good attitude')
-        .click(cy.get('[data-test="add-item"]'));
-      cy.get('[data-test="items-unpacked"] li').last().contains('Good attitude');
+      const item = 'Good Attitude';
+
+      cy.get('[data-test="new-item-input"]').type(item);
+      cy.get('form').submit();
+
+      cy.get('[data-test="items-unpacked"] li').last().contains(item);
     });
   });
 
   describe('Filtering items', () => {
     it('should show items that match whatever is in the filter field', () => {
       cy.get('[data-test="filter-items"]').type('iPhone');
+
       cy.get('[data-test="items"]').contains('iPhone Charger');
     });
 
     it('should hide items that do not match whatever is in the filter field', () => {
       cy.get('[data-test="filter-items"]').type('Hoodie');
-      cy.get('[data-test="items"]').should('not.contain', 'Tooth Brush');
-      cy.get('[data-test="items"]').should('not.contain', 'Tooth Paste');
-      cy.get('[data-test="items"]').should('not.contain', 'Deoderant');
-      cy.get('[data-test="items"]').should('not.contain', 'iPhone Charger');
+      cy.contains('Tooth Brush').should('not.exist');
+      cy.contains('Tooth Paste').should('not.exist');
+      cy.contains('Deoderant').should('not.exist');
+      cy.contains('iPhone Charger').should('not.exist');
     });
   });
 
@@ -49,48 +55,54 @@ describe('Basic Practice', () => {
     describe('Remove all', () => {
       it('should remove all of the items', () => {
         cy.get('[data-test="remove-all"]').click();
-        cy.get('[data-test="items-unpacked"] ul').contains('No items to show');
-        cy.get('[data-test="items-packed"] ul').contains('No items to show');
-        cy.get('[data-test="items-unpacked"] ul li').should('not.exist');
-        cy.get('[data-test="items-packed"] ul li').should('not.exist');
+        cy.get('[data-test="items"] li').should('not.exist');
       });
     });
     describe('Remove individual items', () => {
       it('should have a remove button on an item', () => {
-        cy.get('[data-test="items-packed"] ul li').first().get('button').contains('Remove');
+        cy.get('[data-test="items"] li').each((li) => {
+          cy.wrap(li).find('[data-test="remove"]').should('exist');
+        });
       });
+
       it('should remove an item from the page', () => {
-        cy.get('[data-test="items-unpacked"] ul li')
+        cy.get('[data-test="items"] li')
           .first()
-          .get('button')
-          .contains('Remove')
-          .click();
-        cy.get('[data-test="items-unpacked"] ul li').contains('Tooth Brush').should('not.exist');
+          .within(() => cy.get('[data-test="remove"]').click())
+          .should('not.exist');
       });
     });
   });
 
   describe('Mark all as unpacked', () => {
     it('should empty out the "Packed" list', () => {
-      cy.get('[data-test="items-packed"] ul li');
+      cy.get('[data-test="items-packed"] li');
       cy.get('[data-test="mark-all-as-unpacked"]').click();
-      cy.get('[data-test="items-packed"] ul li').should('not.exist');
+      cy.get('[data-test="items-packed"] li').should('not.exist');
       cy.get('[data-test="items-packed"] ul').contains('No items to show');
     });
 
     it('should have all of the items in the "Unpacked" list', () => {
-      cy.get('[data-test="items-unpacked"] ul li').should('have.length', 4);
-      cy.get('[data-test="mark-all-as-unpacked"]').click();
-      cy.get('[data-test="items-unpacked"] ul li').should('have.length', 5);
+      cy.get('[data-test="items"] li')
+        .its('length')
+        .then((count) => {
+          cy.get('[data-test="mark-all-as-unpacked"]').click();
+          cy.get('[data-test="items-unpacked"] li').its('length').should('equal', count);
+        });
     });
   });
 
   describe('Mark individual item as packed', () => {
     it('should move an individual item from "Unpacked" to "Packed"', () => {
-      cy.get('[data-test="items-unpacked"]').contains('Tooth Brush');
-      cy.get('#item-1 ').click();
-      cy.get('[data-test="items-packed"]').contains('Tooth Brush');
-      cy.get('[data-test="items-unpacked"]').should('not.contain', 'Tooth Brush');
+      cy.get('[data-test="items-unpacked"] li label')
+        .first()
+        .within(() => {
+          cy.get('input[type="checkbox"]').click();
+        })
+        .then(($item) => {
+          const text = $item.text();
+          cy.get('[data-test="items-packed"] li label').first().should('have.text', text);
+        });
     });
   });
 });
