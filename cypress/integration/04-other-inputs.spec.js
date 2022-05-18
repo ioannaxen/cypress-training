6d1d9d836f5ba74f7e9a8bbec97c95cd4094be35
ioannaxen <github@ioanna.dev>
2022-05-18 14:50:44 +0100

    :white_check_mark: Adding input tests

--- cypress/integration/04-other-inputs.spec.js
+++ cypress/integration/04-other-inputs.spec.js
@@ -8,9 +8,22 @@ describe('Secret Menu Items', () => {
     cy.get('#restaurant-visibility-filter').as('restaurant-filter');
   });
 
-  it('should set the range and verify it', () => {});
+  it('should set the range and verify it', () => {
+    cy.get('@rating-filter').should('have.value', '1');
+    cy.get('@rating-filter').invoke('val', '6').trigger('input');
+    cy.get('@rating-filter').should('have.value', '6');
+  });
 
-  it('should check the checkbox and verify it', () => {});
+  it('should check the checkbox and verify it', () => {
+    cy.get('input[type="checkbox"]').first().as('firstCheckbox');
+    cy.get('@firstCheckbox').should('be.checked');
+    cy.get('@firstCheckbox').uncheck();
+    cy.get('@firstCheckbox').should('not.be.checked');
+  });
 
-  it('should select an option from the select and verify it', () => {});
+  it('should select an option from the select and verify it', () => {
+    cy.get('@restaurant-filter').should('not.have.value', 'KFC');
+    cy.get('@restaurant-filter').select('KFC');
+    cy.get('@restaurant-filter').should('have.value', 'KFC');
+  });
 });
--- cypress/integration/05-input-obstacles.spec.js
+++ cypress/integration/05-input-obstacles.spec.js
@@ -13,38 +13,41 @@ describe('Input obstacles', () => {
   });
 
   it('should control a select input', () => {
-    cy.get('[data-test="select-input"]');
-    cy.get('[data-test="select-result"]');
+    const favouriteAvenger = 'Hulk';
+    cy.get('[data-test="select-input"]').select(favouriteAvenger);
+    cy.get('[data-test="select-result"]').contains(favouriteAvenger);
   });
 
   it('should find and control a checkbox input', () => {
-    cy.get('[data-test="checkbox-tomato"]');
-    cy.get('[data-test="checkbox-result"]').contains('(None)');
-    cy.get('[data-test="checkbox-result"]');
+    cy.get('[data-test="checkbox-result"]').as('checkboxResult').contains('(None)');
+    cy.get('[data-test="checkbox-tomato"]').check();
+    cy.get('@checkboxResult').contains('Tomato');
+    cy.get('[data-test="checkbox-onion"]').check();
+    cy.get('@checkboxResult').contains('Tomato, Onion');
   });
 
   it('should find and control a radio input', () => {
-    cy.get('[data-test="radio-ringo"]');
-    cy.get('[data-test="radio-result"]');
+    cy.get('[data-test="radio-ringo"]').check();
+    cy.get('[data-test="radio-result"]').contains('Ringo');
   });
 
   it('should find and control a color input', () => {
-    cy.get('[data-test="color-input"]');
-    cy.get('[data-test="color-result"]');
+    cy.get('[data-test="color-input"]').invoke('val', '#123456').trigger('input');
+    cy.get('[data-test="color-result"]').contains('#123456');
   });
 
   it('should find and control a date input', () => {
-    cy.get('[data-test="date-input"]');
-    cy.get('[data-test="date-result"]');
+    cy.get('[data-test="date-input"]').type('1991-07-25');
+    cy.get('[data-test="date-result"]').contains('1991-07-25');
   });
 
   it('should find and control a range input', () => {
-    cy.get('[data-test="range-input"]');
-    cy.get('[data-test="range-result"]');
+    cy.get('[data-test="range-input"]').invoke('val', '8').trigger('input');
+    cy.get('[data-test="range-result"]').contains('8');
   });
 
   it('should find and control a file input', () => {
-    cy.get('[data-test="file-input"]');
-    cy.get('[data-test="file-result"]');
+    cy.get('[data-test="file-input"]').selectFile('cypress/fixtures/bulbasaur.json');
+    cy.get('[data-test="file-result"]').contains('C:\\fakepath\\bulbasaur.json');
   });
 });
