919097eec165324370a68088e15f8a01d5fa0090
ioannaxen <github@ioanna.dev>
2022-05-19 16:48:58 +0100

    :white_check_mark: Adding location and form validation tests

--- cypress/integration/07-echo-chamber.spec.js
+++ cypress/integration/07-echo-chamber.spec.js
@@ -9,21 +9,57 @@ describe('Initial Page', () => {
     cy.get('[data-test="application-title"]').should('contain', 'Echo Chamber');
   });
 
-  it('should have the title of the application in the window', () => {});
+  it('should have the title of the application in the window', () => {
+    cy.title().should('contain', 'Echo Chamber');
+  });
 
-  it('should navigate to "/sign-in" when you click the "Sign In" button', () => {});
+  it('should navigate to "/sign-in" when you click the "Sign In" button', () => {
+    cy.get('[data-test="sign-in"]').click();
+    cy.location('pathname').should('equal', '/echo-chamber/sign-in');
+  });
 
-  it('should navigate to "/sign-up" when you click the "Sign Up" button', () => {});
+  it('should navigate to "/sign-up" when you click the "Sign Up" button', () => {
+    cy.get('[data-test="sign-up"]').click();
+    cy.location('pathname').should('equal', '/echo-chamber/sign-up');
+  });
 });
 
 describe('Sign Up', () => {
   beforeEach(() => {
     cy.visit('/echo-chamber/sign-up');
+    cy.get('[data-test="sign-up-submit"]').as('submit');
   });
 
-  it('should require an email', () => {});
+  it('should require an email', () => {
+    cy.get('@submit').click();
+    cy.get('[data-test="sign-up-email"]:invalid').should('have.length', 1);
 
-  it('should require that the email actually be an email address', () => {});
+    cy.get('[data-test="sign-up-email"]:invalid')
+      .invoke('prop', 'validationMessage')
+      .should('contain', 'Please fill in this field.');
 
-  it('should require a password when the email is present', () => {});
+    cy.get('[data-test="sign-up-email"]')
+      .invoke('prop', 'validity')
+      .its('valueMissing')
+      .should('be.true');
+  });
+
+  it('should require that the email actually be an email address', () => {
+    cy.get('[data-test="sign-up-email"]').type('hello');
+    cy.get('@submit').click();
+
+    cy.get('[data-test="sign-up-email"]')
+      .invoke('prop', 'validity')
+      .its('typeMismatch')
+      .should('be.true');
+  });
+
+  it('should require a password when the email is present', () => {
+    cy.get('[data-test="sign-up-email"]').type('hello@fairhq.co{enter}');
+
+    cy.get('[data-test="sign-up-password"]')
+      .invoke('prop', 'validity')
+      .its('valueMissing')
+      .should('be.true');
+  });
 });
--- cypress/integration/08-form-validation.spec.js
+++ cypress/integration/08-form-validation.spec.js
@@ -3,11 +3,39 @@
 describe('Sign Up', () => {
   beforeEach(() => {
     cy.visit('/echo-chamber/sign-up');
+    cy.get('[data-test="sign-up-submit"]').as('submit');
   });
 
-  it('should require an email', () => {});
+  it('should require an email', () => {
+    cy.get('@submit').click();
+    cy.get('[data-test="sign-up-email"]:invalid').should('have.length', 1);
 
-  it('should require that the email actually be an email address', () => {});
+    cy.get('[data-test="sign-up-email"]:invalid')
+      .invoke('prop', 'validationMessage')
+      .should('contain', 'Please fill in this field.');
 
-  it('should require a password when the email is present', () => {});
+    cy.get('[data-test="sign-up-email"]')
+      .invoke('prop', 'validity')
+      .its('valueMissing')
+      .should('be.true');
+  });
+
+  it('should require that the email actually be an email address', () => {
+    cy.get('[data-test="sign-up-email"]').type('hello');
+    cy.get('@submit').click();
+
+    cy.get('[data-test="sign-up-email"]')
+      .invoke('prop', 'validity')
+      .its('typeMismatch')
+      .should('be.true');
+  });
+
+  it('should require a password when the email is present', () => {
+    cy.get('[data-test="sign-up-email"]').type('hello@fairhq.co{enter}');
+
+    cy.get('[data-test="sign-up-password"]')
+      .invoke('prop', 'validity')
+      .its('valueMissing')
+      .should('be.true');
+  });
 });
