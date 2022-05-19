9f5f93d63d1bf139e8aa3e6c795b8f91f50d3593
ioannaxen <github@ioanna.dev>
2022-05-19 12:10:06 +0100

    :white_check_mark: Adding automatically generated input tests

--- cypress/integration/06-secret-menu.spec.js
+++ cypress/integration/06-secret-menu.spec.js
@@ -39,4 +39,46 @@ describe('Secret Menu Items', () => {
   it('should exist have the title on the page', () => {
     cy.get('h1').should('contain', 'Secret Menu Items');
   });
+
+  describe('Columns filter', () => {
+    for (const property of properties) {
+      it(`should have a column for ${property}`, () => {
+        cy.get(`#${property}-column`);
+      });
+
+      it(`should hide the ${property} column if unchecked`, () => {
+        cy.get(`#show-${property}`).uncheck();
+        cy.get(`#${property}-column`).should('be.hidden');
+      });
+    }
+  });
+
+  describe('Restaurant filter', () => {
+    beforeEach(() => {
+      cy.get('#restaurant-visibility-filter').as('restaurantFilter');
+    });
+
+    for (const restaurant of restaurants) {
+      it(`should only display rows that match ${restaurant} when selected`, () => {
+        cy.get('@restaurantFilter').select(restaurant);
+        cy.get('td[headers="whereToOrder-column"]').each((item) => {
+          cy.wrap(item).contains(restaurant);
+        });
+      });
+    }
+  });
+  describe('Ratings filter', () => {
+    beforeEach(() => {
+      cy.get('#minimum-rating-visibility').as('ratingFilter');
+    });
+
+    for (const rating of ratings) {
+      it(`should only display items with a popularity above ${rating}`, () => {
+        cy.get('@ratingFilter').invoke('val', rating).trigger('input');
+        cy.get('td[headers="popularity-column"]').each((item) => {
+          expect(+item.text()).to.be.gte(rating);
+        });
+      });
+    }
+  });
 });
