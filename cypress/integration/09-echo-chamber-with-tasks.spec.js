0d971c4f64486620c2ee41bda83a6d90c21c406d
ioannaxen <github@ioanna.dev>
2022-05-19 17:31:31 +0100

    :white_check_mark: Adding tests with tasks

--- cypress/integration/09-echo-chamber-with-tasks.spec.js
+++ cypress/integration/09-echo-chamber-with-tasks.spec.js
@@ -1,11 +1,15 @@
 /// <reference types="cypress" />
 
 const user = {
-  email: `${Date.now()}@example.com`,
+  email: 'first@example.com',
   password: 'password123',
 };
 
 describe('Sign Up', () => {
+  beforeEach(() => {
+    cy.task('reset');
+  });
+
   it('should successfully create a user when entering an email and a password', () => {
     // Sign Up
     cy.visit('/echo-chamber/sign-up');
@@ -15,6 +19,39 @@ describe('Sign Up', () => {
 
     // Sign In
     cy.visit('/echo-chamber/sign-in');
+    cy.get('[data-test="sign-in-email"]').type(user.email);
+    cy.get('[data-test="sign-in-password"]').type(user.password);
+    cy.get('[data-test="sign-in-submit"]').click();
+
+    cy.location('pathname').should('contain', '/echo-chamber/posts');
+    cy.contains('Signed in as ' + user.email);
+  });
+});
+
+describe('Sign In (Failure Mode)', () => {
+  beforeEach(() => {
+    cy.task('reset');
+    cy.visit('/echo-chamber/sign-in');
+  });
+
+  it('should sign in with an existing user', () => {
+    cy.get('[data-test="sign-in-email"]').type(user.email);
+    cy.get('[data-test="sign-in-password"]').type(user.password);
+    cy.get('[data-test="sign-in-submit"]').click();
+
+    cy.location('pathname').should('contain', '/echo-chamber/sign-in');
+    cy.contains('Signed in as ' + user.email).should('not.exist');
+    cy.contains('No such user exists');
+  });
+});
+
+describe('Sign In', () => {
+  beforeEach(() => {
+    cy.task('seed');
+    cy.visit('/echo-chamber/sign-in');
+  });
+
+  it('should sign in with an existing user', () => {
     cy.get('[data-test="sign-in-email"]').type(user.email);
     cy.get('[data-test="sign-in-password"]').type(user.password);
     cy.get('[data-test="sign-in-submit"]').click();
