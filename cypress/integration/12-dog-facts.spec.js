4431829fde6b991ccb9a43eba87ff6663a8461c4
ioannaxen <github@ioanna.dev>
2022-06-29 16:08:00 +0300

    :white_check_mark: Adding more intercept tests

--- cypress/integration/12-dog-facts.spec.js
+++ cypress/integration/12-dog-facts.spec.js
@@ -16,13 +16,34 @@ describe('Dog Facts', () => {
     cy.get('@emptyState');
   });
 
-  it('should make a request when the button is called', () => {});
+  it('should make a request when the button is called', () => {
+    cy.get('@fetchButton').click();
+    cy.wait('@api');
+  });
 
-  it('should adjust the amount when the select is changed', () => {});
+  it('should adjust the amount when the select is changed', () => {
+    cy.get('@amountSelect').select('4');
+    cy.get('@fetchButton').click();
+    cy.wait('@api').its('request.url').should('contain', 'amount=4');
+  });
 
-  it('should show the correct number of facts on the page', () => {});
+  it('should show the correct number of facts on the page', () => {
+    cy.get('@amountSelect').select('4');
+    cy.get('@fetchButton').click();
+    cy.get('[data-test="dog-fact"]').should('have.length', 4);
+  });
 
-  it('should clear the facts when the "Clear" button is pressed', () => {});
+  it('should clear the facts when the "Clear" button is pressed', () => {
+    cy.get('@amountSelect').select('4');
+    cy.get('@fetchButton').click();
+    cy.get('@emptyState').should('not.exist');
+    cy.get('@clearButton').click();
+    cy.get('@emptyState');
+  });
 
-  it("should reflect the number of facts we're looking for in the title", () => {});
+  it("should reflect the number of facts we're looking for in the title", () => {
+    cy.get('@amountSelect').select('4');
+    cy.get('@fetchButton').click();
+    cy.title().should('equal', '4 Dog Facts');
+  });
 });
